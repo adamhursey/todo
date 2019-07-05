eadfd8f4d0dd75e9a0a3062bb267adfdeab649c3
Adam Hursey <adamhursey24@gmail.com>
2019-07-05 01:03:30 -0400

    CRUD implementd

--- app.js
+++ app.js
@@ -15,12 +15,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // All TODO's
 app.get('/api/v1/todos', (req, res) => {
   res.status(200).send({
-    sucess: 'true',
+    success: 'true',
     message: 'todos retrieved successfully',
     todos: db
   })
 });
 
+// Single TODO
+app.get('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  let todoFromDb;
+
+  db.map((todo) => {
+    if (todo.id === id) {
+      todoFromDb = todo;
+    }
+  });
+  if (todoFromDb) {
+    return res.status(200).send({
+      success: 'true',
+      message: 'todo retrieved successfully',
+      todoFromDb,
+    });
+  } else {
+    return res.status(404).send({
+      success: 'false',
+      message: 'todo does not exist',
+    });
+  }
+});
 
 // ------------------------- //
 //-------POST Methods------- //
@@ -28,7 +52,7 @@ app.get('/api/v1/todos', (req, res) => {
 
 // Single TODO
 app.post('/api/v1/todos', (req, res) => {
-  if(!req.body.title) {
+  if (!req.body.title) {
     return res.status(400).send({
       success: 'false',
       message: 'title is required'
@@ -40,22 +64,107 @@ app.post('/api/v1/todos', (req, res) => {
     });
   }
   const todo = {
-    id: db.length +1,
-    title:req.body.title,
+    id: db.length + 1,
+    title: req.body.title,
     description: req.body.description
   }
   db.push(todo);
   return res.status(201).send({
     success: 'true',
-    messag: 'todo added successfully',
+    message: 'todo added successfully',
     todo
   })
 });
 
+// --------------------------- //
+//-------DELETE Methods------- //
+// --------------------------- //
 
+//Delete certian TODO
+app.delete('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  let deleted;
+
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      db.splice(index, 1);
+      deleted = true;
+    }
+  });
+
+    if (deleted) {
+      return res.status(200).send({
+        success: 'true',
+        message: 'Todo deleted successfuly',
+      });
+    } else {
+      return res.status(404).send({
+        success: 'false',
+        message: 'todo not found',
+      });
+}
+
+});
+
+// --------------------------- //
+//-------UPDATE Methods------- //
+// --------------------------- //
+
+// PUT Method
+app.put('/api/v1/todos/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+  let todoFound;
+  let itemIndex;
+  db.map((todo, index) => {
+    if (todo.id === id) {
+      todoFound = todo;
+      itemIndex = index;
+    }
+  });
+
+  if (!todoFound) {
+    return res.status(404).send({
+      success: 'false',
+      message: 'todo not found',
+    });
+  }
+
+  if (!req.body.title) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required',
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required',
+    });
+  }
+
+  const updatedTodo = {
+    id: todoFound.id,
+    title: req.body.title || todoFound.title,
+    description: req.body.description || todoFound.description,
+  };
+
+  db.splice(itemIndex, 1, updatedTodo);
+
+  return res.status(201).send({
+    success: 'true',
+    message: 'todo updated successfully',
+    updatedTodo,
+  });
+});
+
+
+
+// -------------------------------- //
+//-------CREATING WEB SERVER------- //
+// -------------------------------- //
 
 const PORT = 5000;
 
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
