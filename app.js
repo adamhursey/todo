ab7c34458536a85c61d16834875d878cc5aaae54
Adam Hursey <adamhursey24@gmail.com>
2019-07-04 23:46:55 -0400

    added post method

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import db from './db/db';
 
 const app = express();
 
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// ------------------------ //
+//-------GET Methods------- //
+// ------------------------ //
+
+// All TODO's
 app.get('/api/v1/todos', (req, res) => {
   res.status(200).send({
     sucess: 'true',
@@ -10,6 +20,39 @@ app.get('/api/v1/todos', (req, res) => {
     todos: db
   })
 });
+
+
+// ------------------------- //
+//-------POST Methods------- //
+// ------------------------- //
+
+// Single TODO
+app.post('/api/v1/todos', (req, res) => {
+  if(!req.body.title) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'title is required'
+    });
+  } else if (!req.body.description) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'description is required'
+    });
+  }
+  const todo = {
+    id: db.length +1,
+    title:req.body.title,
+    description: req.body.description
+  }
+  db.push(todo);
+  return res.status(201).send({
+    success: 'true',
+    messag: 'todo added successfully',
+    todo
+  })
+});
+
+
 
 const PORT = 5000;
 
