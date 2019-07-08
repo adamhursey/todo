79cc667ed082c974200a4fcb3337075c025b519e
Adam Hursey <adamhursey24@gmail.com>
2019-07-07 23:05:28 -0400

    part way through adding db

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,8 @@ import db from '../db/db';
 const router = express.Router();
 
 router.get('/api/v1/todos', TodoContoller.getAllTodos);
-
 router.get('/api/v1/todos/:id', TodoContoller.getTodo);
-
-
 router.post('/api/v1/todos', TodoContoller.createTodo);
-
 router.delete('/api/v1/todos/:id', TodoContoller.deleteTodo);
 router.put('/api/v1/todos/:id', TodoContoller.updateTodo);
 
